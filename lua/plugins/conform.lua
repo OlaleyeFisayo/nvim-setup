e8621fc1d6af39a22e26c844c7504d8567535a52
OlaleyeFisayo <festusolaleyef@gmail.com>
2025-06-21 13:25:29 +0100

    fix: conform.lua, to format with eslint on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,22 +2,22 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      javascript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescript = { "eslint" },
-      typescriptreact = { "eslint" },
-      vue = { "eslint" },
+      javascript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      vue = { "eslint_d" },
     },
     formatters = {
-      eslint = {
+      eslint_d = {
         condition = function(ctx)
           return vim.fs.find(
-            { ".eslintrc.js", ".eslintrc.js", ".eslintrc.json", ".eslintrc", "eslint.config.js", "eslint.config.mjs" },
+            { ".eslintrc.js", ".eslintrc.json", ".eslintrc", "eslint.config.js", "eslint.config.mjs" },
             { path = ctx.dirname, upward = true }
           )[1] ~= nil
         end,
       },
-    },
+    -- },
     format_on_save = {
       lsp_fallback = true,
     },
